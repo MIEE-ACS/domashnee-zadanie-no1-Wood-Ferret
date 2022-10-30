96f5ef653c11fa0dea315e6cfd57cf8c97618a35
Wood-Ferret <113302435+Wood-Ferret@users.noreply.github.com>
2022-10-31 01:52:00 +0300

    Белый А УТС-21 вар 2
    Здравствуйте, оказывается код просто не был залит, также нашел нужные вопросы
    Static используется при объявлении методов, переменных и классов в языке и обозначает вещи, которые уже не могут быть повторены. Может быть использован для увеличения функционала ну и в целом обозначить что-то за одно неизменным для других классов.
    string[] args - это параметры запуска приложения из командной строки

---
+++ Дз 1/Дз 1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Дз_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            const double Pi = 3.14;
+
+            Console.WriteLine("Введите длину окружности L: ");
+            int n = 0;
+
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out n))//проверка значения
+                {
+                    if (n > 0)
+                    {
+                        break;
+                    }
+                    else Console.WriteLine("L должен быть положительным числом");
+                }
+            }
+            try
+            {
+                double L = Convert.ToDouble(Console.ReadLine());
+
+                double R = L / (2 * Pi);
+                Console.WriteLine(String.Format(" Радиус: {0:F3} \n Площадь: {1:F3}", R, Pi * R * R));
+
+            }
+            catch (FormatException e)
+            {
+                Console.Error.WriteLine("Длина окружности введена неверно, введите число ");
+            }
+        }
+    }
+}
